18402e56fa7c4ede7c86f0e20aee86d6c872c71a
João Pedro <jotapedro1231@gmail.com>
2023-07-20 15:34:35 -0300

    Display weather icon

--- src/components/WeatherInfo/WeatherInfo.tsx
+++ src/components/WeatherInfo/WeatherInfo.tsx
@@ -9,9 +9,15 @@ interface WeatherInfoProps {
 }
 
 export function WeatherInfo({ weatherData }: WeatherInfoProps) {
+  const icon = weatherData?.weather[0]?.icon
+
   return (
     <div className="weather-info">
-      <p>{weatherData?.main.temp}</p>
+      <p>
+        {weatherData?.weather[0].icon && (
+          <img src={`http://openweathermap.org/img/wn/${icon}.png`} alt="" />
+        )}
+      </p>
     </div>
   )
 }

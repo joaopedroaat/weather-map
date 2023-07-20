75f6cdf03eab2d3c95f4998ff39759f370813af3
João Pedro <jotapedro1231@gmail.com>
2023-07-20 14:41:27 -0300

    Max bounds and no wrap

--- src/components/GlobeMap/GlobeMap.tsx
+++ src/components/GlobeMap/GlobeMap.tsx
@@ -11,11 +11,18 @@ export function GlobeMap({ handleLocaleChange }: GlobeMapProps) {
   return (
     <MapContainer
       center={[0, 0]}
+      minZoom={3}
       zoom={3}
       scrollWheelZoom={true}
       style={{ height: '100%', cursor: 'default' }}
+      maxBounds={[
+        [-90, -180],
+        [90, 180],
+      ]}
+      maxBoundsViscosity={1}
     >
       <TileLayer
+        noWrap={true}
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />

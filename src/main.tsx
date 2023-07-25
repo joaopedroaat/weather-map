1741b3ae67e2178b27463f06da5c5712f7b3f9f3
João Pedro <jotapedro1231@gmail.com>
2023-07-25 14:43:30 -0300

    Removed global.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
-import './global.css'
-
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
